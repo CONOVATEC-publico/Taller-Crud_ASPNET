1b6148f2da623b6d516f0c29c5dc097a403fb8d6
Adrian <adrian.capchaq@gmail.com>
2022-12-29 18:08:52 -0500

    agregando sesion 2

---
+++ Sesion2/Sesion1/Controllers/productoController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sesion1;
+
+namespace Sesion1.Controllers
+{
+    public class productoController : Controller
+    {
+        private tiendaEntities db = new tiendaEntities();
+
+        // GET: producto
+        public ActionResult Index()
+        {
+            var producto = db.producto.Include(p => p.categoria).Include(p => p.estado);
+            return View(producto.ToList());
+        }
+        //Post:Consultar cursor por nombre LINQ
+        /*  [HttpPost]
+          public ActionResult Index(string nombre)
+          {
+              var consulta = from p in db.producto
+                             where p.nombre == nombre
+                             select p;
+              return View(consulta);
+          }*/
+        //con expresiones Lambda
+        [HttpPost]
+        public ActionResult Index(string nombre)
+        {
+            var consulta = db.producto.Where(n => n.nombre.Contains(nombre)).ToList();
+            return View(consulta);
+        }
+
+
+
+        // GET: producto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producto producto = db.producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: producto/Create
+        public ActionResult Create()
+        {
+            ViewBag.idcategoria = new SelectList(db.categoria, "idcategoria", "descripcion");
+            ViewBag.idestado = new SelectList(db.estado, "idestado", "descripcion");
+            return View();
+        }
+
+        // POST: producto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idproducto,nombre,precio,stock,idcategoria,idestado")] producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.producto.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idcategoria = new SelectList(db.categoria, "idcategoria", "descripcion", producto.idcategoria);
+            ViewBag.idestado = new SelectList(db.estado, "idestado", "descripcion", producto.idestado);
+            return View(producto);
+        }
+
+        // GET: producto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producto producto = db.producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idcategoria = new SelectList(db.categoria, "idcategoria", "descripcion", producto.idcategoria);
+            ViewBag.idestado = new SelectList(db.estado, "idestado", "descripcion", producto.idestado);
+            return View(producto);
+        }
+
+        // POST: producto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idproducto,nombre,precio,stock,idcategoria,idestado")] producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idcategoria = new SelectList(db.categoria, "idcategoria", "descripcion", producto.idcategoria);
+            ViewBag.idestado = new SelectList(db.estado, "idestado", "descripcion", producto.idestado);
+            return View(producto);
+        }
+
+        // GET: producto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producto producto = db.producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            producto producto = db.producto.Find(id);
+            db.producto.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sesion2/Sesion1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sesion1.MvcApplication" Language="C#" %>
